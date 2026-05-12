8b85031d2278299a5e1db415b700373e0b0599cd
luizuehara1 <luiz.uehara1@gmail.com>
2026-05-12 03:09:48 -0300

    feat: Migrate Electron main process to CJS
    Updates the main Electron process file from `.js` to `.cjs` to align with common practices for Node.js modules and ensure compatibility with module resolution. Also, updates the `package.json` to point to the new `.cjs` main file.

--- electron/main.cjs
+++ electron/main.cjs
@@ -63,7 +63,7 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(__dirname, 'preload.cjs'),
     },
     backgroundColor: '#00000000',
   });
